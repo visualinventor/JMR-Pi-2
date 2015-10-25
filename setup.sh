1f03b141d52d2ddb47cef732b78b057eea93b6ca
Tim Watson <twatson@opubco.com>
2015-10-25 14:38:37 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# JMR-Pi - Copyright Matthew Macdonald-Wallace 2012
+# JMR-Pi   - Copyright Matthew Macdonald-Wallace 2012
+# JMR-Pi 2 - Copyright Tim Watson 2015
 
 JMRI_URL=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 5,6 | cut -d " " -f 2 | cut -d '"' -f 2)
 JMRI_PACKAGE_NAME=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 6 | cut -d "/" -f 8)
@@ -48,12 +49,6 @@ then
   error "Failed to install dependencies"
 fi
 
-
-####### Uncomment this if you've changed the script to use a much older version that still requires the RXTX Hack
-#cd /opt/JMRI/lib/linux/armv5
-#mv librxtxSerial.so librxtxSerial.so.jmri
-#ln -s /usr/lib/jni/librxtxSerial.so
-
 # create the jmri user that we will run as:
 useradd -m -s /bin/bash -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,netdev,input jmri
 echo -e "jmri:trains" | (chpasswd)
@@ -79,35 +74,35 @@ echo -e "trains\ntrains" | (smbpasswd -a -s jmri)
 
 # copy the files to the correct location and set permissions:
 cp $WORKING_DIR/scripts/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
-# cp $WORKING_DIR/scripts/init.d/vncserver /etc/init.d/vncserver
+cp $WORKING_DIR/scripts/init.d/vncserver /etc/init.d/vncserver
 if [ ! -f /home/jmri/.jmri/PanelProConfig2.xml ]
 then
   cp $WORKING_DIR/scripts/jmri/PanelProConfig2.xml /home/jmri/.jmri/PanelProConfig2.xml
   ln -s /home/jmri/.jmri/JmriFacelessConfig3.xml /home/jmri/.jmri/PanelProConfig2.xml
 fi
-#chmod +x /etc/init.d/vncserver
+chmod +x /etc/init.d/vncserver
 mkdir -p /home/jmri/.config/lxsession/LXDE
 echo '@/opt/JMRI/PanelPro' >> /home/jmri/.config/lxsession/LXDE/autostart
 chown -Rf jmri: /home/jmri
 chown -Rf jmri: /opt/JMRI
 
 # start the services:
-#/etc/init.d/vncserver start
-#if [ $? -ne 0 ]
-#then
-#  warning "VNC server failed to start"
-#fi
+/etc/init.d/vncserver start
+if [ $? -ne 0 ]
+ then
+  warning "VNC server failed to start"
+fi
 #
 # add the vnc service to start at boot
-#update-rc.d vncserver defaults
+update-rc.d vncserver defaults
 
 # get the current ip addresses
 ip=$(hostname -I)
-echo -e " hostname -I" > test.log
+echo -e "hostname -I" > test.log
 # echo the details:
-echo "### Your JMRI server is ready ###"
-echo "VNC/RDP IPs: $ip Port: 5901"
+echo "---- Your JMRI server has been installed ----"
+echo "To connect through VNC or Remote Desktop use the following IP/port: $ip:5901"
 echo "JMRI will take several minutes to start the first time it is run."
-echo "Your config files should be available by browsing to \\$IPADDRESS\\JMRI\\"
+echo "Your config files should be available by browsing SAMBA to \\$ip\\JMRI\\"
 
 exit 0
