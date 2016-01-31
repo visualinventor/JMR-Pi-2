8ce293a69aa588711a91790032aa8d9ac2ae66dd
visualinventor <tim@visualinventor.com>
2016-01-30 21:59:59 -0600

    I think this is ready for the beta release

--- setup.sh
+++ setup.sh
@@ -32,41 +32,12 @@ fi
 # We need to comment out the no dhcp option
 sed -e '/DHCPD_ENABLED/ s/^#*/#/' -i /etc/default/udhcpd
 
-## Backup interfaces file
-echo "------------- We're going to backup your network/interfaces file so you'll have the original"
-cp /etc/network/interfaces{,.bak}
 
-# With dhcpcd these are all unecessary
-# We need to comment out these if they exist
-#sed -e '/auto wlan0/ s/^#*/#/' -i /etc/network/interfaces
-#sed -e '/allow-hotplug wlan0/ s/^#*/#/' -i /etc/network/interfaces
-#sed -e '/wpa-roam/ s/^#*/#/' -i /etc/network/interfaces
-#sed -e '/wpa-conf/ s/^#*/#/' -i /etc/network/interfaces
-#sed -e '/iface wlan0 inet manual/ s/^#*/#/' -i /etc/network/interfaces
-
-#Add our new static ip address to the pi
-#echo "iface wlan0 inet static" >> /etc/network/interfaces
-#echo "	address 192.168.10.1" >> /etc/network/interfaces
-#echo "	netmask 255.255.255.0" >> /etc/network/interfaces
-#echo "	gateway 192.168.10.1" >> /etc/network/interfaces
-#echo -e "\niface default inet static" >> /etc/network/interfaces
-
-# With dhcpcd these are all we need
+# With dhcpcd these are all we need for a static ip address
 echo "interface wlan0" >> /etc/dhcpcd.conf
 echo "static ip_address=192.168.10.1/24" >> /etc/dhcpcd.conf
 echo "static routers=192.168.10.1" >> /etc/dhcpcd.conf
 echo "static domain_name_servers=192.168.10.1" >> /etc/dhcpcd.conf
-
-# dhcp was running before the static ip was set so we need to make
-# the server start after it
-#cp $WORKING_DIR/conf/wlan/fixnet /etc/network/if-up.d/
-#if [ $? -ne 0 ]
-#then
-#  error "Failed to copy fixnet file"
-#fi
-
-#chmod 755 /etc/network/if-up.d/fixnet
-
 
 cp $WORKING_DIR/conf/hostapd/hostapd.conf  /etc/hostapd/hostapd.conf
 if [ $? -ne 0 ]
@@ -86,9 +57,8 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
  
- # Save IP tables
+# Save IP tables
 sh -c "iptables-save > /etc/iptables.ipv4.nat"
-
 echo "up iptables-restore < /etc/iptables.ipv4.nat" >> /etc/network/interfaces
 
 # Start them up
@@ -100,16 +70,17 @@ update-rc.d udhcpd defaults
 
 #### End wi-fi hotspot setup
 
+
 # change name from default hostname
-echo "--------- Setting hostname to jmrpi2"
+echo "------------- Setting hostname to jmrpi2"
 sed -e '/127.0.0.1	raspberrypi/ s/^#*/#/' -i /etc/hosts
 echo -e '127.0.0.1\tjmrpi2' >> /etc/hosts
 sed --in-place '/raspberrypi/d' /etc/hostname
 echo 'jmrpi2' >> /etc/hostname
-
 hostname jmrpi2
 
 ## Installing a JMRI 4 or greater compatible java with rxtx library:
+echo "------------- Installing a JMRI 4 or greater compatible java with rxtx library"
 apt-get -y install oracle-java8-jdk librxtx-java xrdp
 if [ $? -ne 0 ]
 then
@@ -117,6 +88,7 @@ then
 fi
 
 ## DOWNLOAD the various JMRI packages we need
+echo "------------- DOWNLOAD the various JMRI packages we need"
 JMRI_URL=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 5,6 | cut -d " " -f 2 | cut -d '"' -f 2)
 JMRI_PACKAGE_NAME=$(curl -s http://jmri.org/releaselist -o - | tr '\n' ' ' | cut -d ":" -f 6 | cut -d "/" -f 8)
 
@@ -162,7 +134,6 @@ groupadd -r autologin
 groupadd -r nopasswdlogin
 
 # create the jmri user that we will run as:
-#useradd -mG autologin,nopasswdlogin,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,netdev,input -s /bin/bash jmri
 useradd -m -s /bin/bash -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,netdev,input jmri
 echo -e "jmri:trains" | (sudo chpasswd)
 
@@ -172,8 +143,8 @@ gpasswd -a jmri nopasswdlogin
 #pam.d needs to know to auto load the nopasswdlogin group - https://wiki.archlinux.org/index.php/LightDM
 echo "auth        sufficient  pam_succeed_if.so user ingroup nopasswdlogin" | sudo tee -a /etc/pam.d/lightdm
 
-
 # install SAMBA and configure a file server:
+echo "------------- Install SAMBA and configure a file server"
 apt-get -y install samba samba-common-bin
 if [ $? -ne 0 ]
 then
@@ -205,7 +176,7 @@ if [ $? -ne 0 ]
  then
   warning "VNC server failed to start"
 fi
-#
+
 # add the vnc service to start at boot
 update-rc.d tightvncserver defaults
 
@@ -213,14 +184,8 @@ update-rc.d tightvncserver defaults
 mkdir /home/jmri/.jmri
 chown -Rf jmri: /home/jmri/.jmri
 
-#if [ ! -f /home/jmri/.jmri/PanelProConfig2.properties ]
-#then
-#  cp $WORKING_DIR/confs/jmri/PanelProConfig2.properties /home/jmri/.jmri/PanelProConfig2.properties
-#  ln -s /home/jmri/.jmri/JmriFacelessConfig3.properties /home/jmri/.jmri/PanelProConfig2.properties
-#fi
-
-
 mkdir -p /home/jmri/.config/lxsession/LXDE-pi
+
 # To run a more limited version of JMRI (Faceless) comment out the below line and uncomment the one below it
 echo '@/opt/JMRI/PanelPro' >> /home/jmri/.config/lxsession/LXDE-pi/autostart
 #echo '@/opt/JMRI/JmriFaceless' >> /home/jmri/.config/lxsession/LXDE-pi/autostart
@@ -230,7 +195,7 @@ chown -Rf jmri: /opt/JMRI
 
 # get the current ip addresses
 ip=$(hostname -I)
-echo -e "hostname -I" > test.log
+#echo -e "hostname -I" > test.log
 # echo the details:
 echo "---- Your JMRI server has been installed ----"
 echo "To connect through VNC or Remote Desktop use the following IP/port: $ip:5901"
